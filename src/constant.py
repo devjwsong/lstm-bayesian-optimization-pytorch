c094f75552c2dd9cdf34a5471f7d350648d61ea3
devJWSong <enflwodn@gmail.com>
2020-07-06 09:23:01 +0900

    Bayesian Optimization text complete

--- src/constant.py
+++ src/constant.py
@@ -9,8 +9,8 @@ test_name = 'test.txt'
 
 # Parameters for training and modeling
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-learning_rate = 0.0005
-batch_size = 128
+learning_rates = (0.0001, 0.001)
+batch_sizes = (16, 128)
 seq_len = 512
 d_w = 256
 d_h = 256
@@ -18,8 +18,12 @@ drop_out_rate = 0.5
 layer_num = 3
 bidirectional = True
 class_num = 5
-epoch_num = 15
+epoch_num = 10
 ckpt_dir = '../saved_model'
+
+# Parameters for Bayesian Optimization
+init_points = 2
+n_iter = 8
 
 # Path for tensorboard
 summary_path = '../runs'
--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@ from custom_data import *
 from lstm import *
 from constant import *
 from torch.utils.data import DataLoader
-from torch.utils.tensorboard import SummaryWriter
 from sklearn.metrics import f1_score
+from bayes_opt import BayesianOptimization
 
 import torch
 import torch.optim as optim
@@ -15,46 +15,49 @@ import numpy as np
 
 
 class Manager:
-    def __init__(self, mode, model_name=None):
+    def __init__(self):
         print("Loading dataset & vocab dict...")
-        train_set, dev_set, test_set, word2idx = get_data()
-        self.train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
-        self.valid_loader = DataLoader(dev_set, batch_size=batch_size, shuffle=True)
-        self.test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=True)
+        self.train_set, self.dev_set, self.test_set, self.word2idx = get_data()
+
+        self.bayes_optimizer = BayesianOptimization(
+            f=self.train,
+            pbounds={
+                'learning_rate': learning_rates,
+                'batch_size': batch_sizes
+            },
+            random_state=777
+        )
+
+    def train(self, learning_rate, batch_size):
+        batch_size = round(batch_size)
+        train_loader = DataLoader(self.train_set, batch_size=batch_size, shuffle=True)
+        valid_loader = DataLoader(self.dev_set, batch_size=batch_size, shuffle=True)
 
         print("Loading model...")
-        self.model = LSTM(len(word2idx))
+        model = LSTM(len(self.word2idx)).to(device)
+        criterion = nn.NLLLoss(reduction='mean')
 
-        if mode == 'train':
-            if not os.path.isdir(ckpt_dir):
-                os.mkdir(ckpt_dir)
+        if not os.path.isdir(ckpt_dir):
+            os.mkdir(ckpt_dir)
 
-            for p in self.model.parameters():
-                if p.dim() > 1:
-                    nn.init.xavier_uniform_(p)
+        for p in model.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p)
 
-            print("Initializing optimizer & loss function...")
-            self.optim = optim.Adam(self.model.parameters(), lr=learning_rate)
-            self.summary = SummaryWriter()
-        elif mode == 'test':
-            assert model_name is not None, "Please specify the model name if you want to test."
+        print("Initializing optimizer & loss function...")
+        optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-            self.model.load_state_dict(torch.load(f"{ckpt_dir}/{model_name}"))
-
-        self.model = self.model.to(device)
-        self.criterion = nn.NLLLoss(reduction='mean')
-
-    def train(self):
         best_f1 = 0.0
 
+        print("Train starts.")
         for epoch in range(1, epoch_num+1):
-            self.model.train()
+            model.train()
 
             total_train_losses = []
             total_train_preds = []
             total_train_targs = []
 
-            for batch in tqdm(self.train_loader):
+            for batch in tqdm(train_loader):
                 x, y, lens = batch
                 lens_sorted, idx = lens.sort(dim=0, descending=True)
                 x_sorted = x[idx]
@@ -62,12 +65,12 @@ class Manager:
 
                 x, y, lens = x_sorted.to(device), y_sorted.to(device), lens_sorted.to(device)
 
-                output = self.model(x, lens)  # (B, class_num)
-                loss = self.criterion(output, y)  # ()
+                output = model(x, lens)  # (B, class_num)
+                loss = criterion(output, y)  # ()
 
-                self.optim.zero_grad()
+                optimizer.zero_grad()
                 loss.backward()
-                self.optim.step()
+                optimizer.step()
 
                 total_train_losses.append(loss.item())
                 total_train_preds += torch.argmax(output, dim=-1).tolist()
@@ -79,29 +82,24 @@ class Manager:
             print(f"########## Epoch: {epoch} ##########")
             print(f"Train loss: {train_loss} || Train f1 score: {train_f1}")
 
-            valid_loss, valid_f1 = self.validate()
+            valid_loss, valid_f1 = self.validate(model, criterion, valid_loader)
 
             if valid_f1 > best_f1:
                 print("***** Current best model saved. *****")
-                torch.save(self.model.state_dict(), f"{ckpt_dir}/best_model.pth")
+                torch.save(model.state_dict(), f"{ckpt_dir}/best_model_batch|{batch_size}_lr|{round(learning_rate, 4)}.pth")
                 best_f1 = valid_f1
 
             print(f"Valid loss: {valid_loss} || Valid f1 score: {valid_f1} || Best f1 score: {best_f1}")
 
-            self.summary.add_scalar('loss/train_loss', train_loss, epoch)
-            self.summary.add_scalar('loss/validation_loss', valid_loss, epoch)
-            self.summary.add_scalars('loss/loss_group', {'train': train_loss,
-                                                   'validation': valid_loss}, epoch)
+        return best_f1
 
-        self.summary.close()
-
-    def validate(self):
-        self.model.eval()
+    def validate(self, model, criterion,  valid_loader):
+        model.eval()
         total_valid_losses = []
         total_valid_preds = []
         total_valid_targs = []
 
-        for batch in tqdm(self.valid_loader):
+        for batch in tqdm(valid_loader):
             x, y, lens = batch
             lens_sorted, idx = lens.sort(dim=0, descending=True)
             x_sorted = x[idx]
@@ -109,8 +107,8 @@ class Manager:
 
             x, y, lens = x_sorted.to(device), y_sorted.to(device), lens_sorted.to(device)
 
-            output = self.model(x, lens)  # (B, class_num)
-            loss = self.criterion(output, y)  # ()
+            output = model(x, lens)  # (B, class_num)
+            loss = criterion(output, y)  # ()
 
             total_valid_losses.append(loss.item())
             total_valid_preds += torch.argmax(output, dim=-1).tolist()
@@ -121,13 +119,21 @@ class Manager:
 
         return valid_loss, valid_f1
 
-    def test(self):
-        self.model.eval()
+    def test(self, model_name, batch_size):
+        test_loader = DataLoader(self.test_set, batch_size=batch_size, shuffle=True)
+
+        print("Loading model...")
+        model = LSTM(len(self.word2idx))
+        criterion = nn.NLLLoss(reduction='mean')
+
+        model.load_state_dict(torch.load(f"{ckpt_dir}/{model_name}")).to(device)
+
+        model.eval()
         total_test_losses = []
         total_test_preds = []
         total_test_targs = []
 
-        for batch in tqdm(self.test_loader):
+        for batch in tqdm(test_loader):
             x, y, lens = batch
             lens_sorted, idx = lens.sort(dim=0, descending=True)
             x_sorted = x[idx]
@@ -135,8 +141,8 @@ class Manager:
 
             x, y, lens = x_sorted.to(device), y_sorted.to(device), lens_sorted.to(device)
 
-            output = self.model(x, lens)  # (B, class_num)
-            loss = self.criterion(output, y)  # ()
+            output = model(x, lens)  # (B, class_num)
+            loss = criterion(output, y)  # ()
 
             total_test_losses.append(loss.item())
             total_test_preds += torch.argmax(output, dim=-1).tolist()
@@ -158,11 +164,16 @@ if __name__=='__main__':
 
     assert args.mode == 'train' or args.mode == 'test', "Please specify correct mode."
 
-    manager = Manager(args.mode, args.model_name)
+    manager = Manager()
 
     if args.mode == 'train':
         print("Training starts.")
-        manager.train()
+        manager.bayes_optimizer.maximize(init_points=init_points, n_iter=n_iter, acq='ei', xi=0.01)
+
+        print("Best optimization option")
+        print(manager.bayes_optimizer.max)
     elif args.mode == 'test':
+        assert args.model_name is not None, "Please give the model name if you want to conduct test."
+
         print("Testing starts.")
-        manager.test()
+        manager.test(args.model_name, batch_size=128)
